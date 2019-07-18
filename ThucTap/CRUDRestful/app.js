5b9c12c9a781fdc72199fdf667eae8722f03471b
Bùi Thành Long <33962118+buithanhlong16110139@users.noreply.github.com>
2019-07-18 10:38:04 +0700

    update async/await
    update ví dụ về async/await

--- ThucTap/CRUDRestful/app.js
+++ ThucTap/CRUDRestful/app.js
@@ -13,18 +13,18 @@ var router = require('./routes/index');
 //const router = require('./routes');
 //const users = require('./routes/users');
 //const books = require('./routes/books');
- 
+
 var app = express();
- 
+
 // 2
 mongoose.Promise = global.Promise
 const mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/CRUDProduct'
 mongoose.connect(mongoDB)
- 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
- 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -32,21 +32,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
- 
+
 // 3
 app.use(session({
   secret: 'secret',
   saveUninitialized: true,
   resave: true
 }))
- 
+
 //4
 // app.use(expressValidator({
 //   errorFormatter: function(param, msg, value) {
 //     var namespace = param.split('.')
 //     , root = namespace.shift()
 //     , formParam = root
- 
+
 //     while(namespace.length) {
 //       formParam += '[' + namespace.shift() + ']'
 //     }
@@ -57,21 +57,46 @@ app.use(session({
 //     }
 //   }
 // }))
- 
+
 // 5
 app.use(flash())
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')
   next()
 })
- 
+
 // 6
-app.use('/', router);
+//app.use('/', router);
 //app.use('/users', users);
 //app.use('/books', books);
- 
+
+//Bất đồng bộ
+app.get('/', function () {
+  const User = require('./routes/users/user.Models');
+  execute()
+
+  async function execute() {
+    let result = await findResult()
+    console.log("bước 3");///second
+  }
+
+  async function findResult() {
+    for (let i = 0; i < 1000000000; i++) {
+      let j = 100
+    }
+
+    console.log('Bước 1');///first
+
+    let result = await User.collection.findOne({ username: 'long14a12dgl' });
+    console.log("Bước 2");///third
+    console.log(result);
+    //console.log('after findResult');
+    return(result);
+  }
+})
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -81,14 +106,14 @@ app.use(function(req, res, next) {
 //   res.render('addPro');
 // })
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
- 
+
 module.exports = app;
